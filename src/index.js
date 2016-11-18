c75c771202a4534970555437930ff3597dc53faf
justhineT <jusatumb@gmail.com>
2016-11-17 20:47:30 -0500

    Widget structure css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import { Address, LatLng, Warehouse } from './classes/places'
 import { platforms, vehicleTypes } from './constants'
 import errors from './errors'
 
+import bike from './images/icon_v_bike.svg';
+import car from './images/icon_v_car.svg';
+import moto from './images/icon_v_moto.svg';
+import van from './images/icon_v_van.svg';
+import truck from './images/icon_v_truck.svg';
+
+import './style.css';
+
 window.shippify = {
   vehicleTypes,
   errors,
@@ -18,23 +26,22 @@ window.shippify = {
     Widget,
   }
 }
+ const { shippify } = window
 
-// const { shippify } = window
-//
-// const pickupPlace = new shippify.places.Address('Av. 9 de Octubre, Guayaquil, EC')
-// const orderTemplate = {
-//   id: 'my-platform-reference-id',
-//   platform: shippify.integrations.platforms.VTEX,
-//   pickupPlace,
-// }
-// const orderManagerOptions = {
-//   credentials: {
-//     apiId: 'my-shippify-api-id',
-//     apiSecret: 'my-shippify-api-secret'
-//   },
-//   // googleMapsAPIKey: 'my-google-maps-api-key'
-// }
-// const orderManager = new shippify.integrations.OrderManager(orderTemplate, orderManagerOptions)
-//
-// const shippifyWidgetContainer = document.getElementById('root')
-// const widget = new shippify.integrations.Widget(orderManager, shippifyWidgetContainer)
+ const pickupPlace = new shippify.places.Address('Av. 9 de Octubre, Guayaquil, EC')
+ const orderTemplate = {
+   id: 'my-platform-reference-id',
+   platform: shippify.integrations.platforms.VTEX,
+   pickupPlace,
+ }
+ const orderManagerOptions = {
+   credentials: {
+     apiId: 'my-shippify-api-id',
+     apiSecret: 'my-shippify-api-secret'
+   },
+   // googleMapsAPIKey: 'my-google-maps-api-key'
+ }
+ const orderManager = new shippify.integrations.OrderManager(orderTemplate, orderManagerOptions)
+
+const shippifyWidgetContainer = document.getElementById('root')
+const widget = new shippify.integrations.Widget(orderManager, shippifyWidgetContainer)
